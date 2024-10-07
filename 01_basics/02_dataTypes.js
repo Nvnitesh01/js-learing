a5e68c00cc580cdbf9919c87439488fc6f7db9fb
Nvnitesh01 <54271273+Nvnitesh01@users.noreply.github.com>
2024-10-07 09:55:43 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treats all js code as newer js version
+
+// alert ("hello");
+// alert (3 + 3);   we are using nodejs not browser
+
+console.log(3 
+    + 3) // code will run here but this is not the correct way to write the code.. code should be readable.
+console.log(3 + 3); // this is proper way to write code
+
+let name = "Nitesh"
+let age = 35
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof "Nitesh");
+console.log(typeof null); // type = object
+console.log(typeof undefined); // type = undefined
