90469b77f62ff89b60f21eb0cd745ef093b5e3f1
Nvnitesh01 <54271273+Nvnitesh01@users.noreply.github.com>
2024-10-12 07:28:05 +0000

    comparisons

---
+++ 01_basics/04_comparisons.js
@@ -0,0 +1,29 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log("2" == 2);  
+console.log(typeof "2");
+ // javascript converting string "2" into number automatically but as we can see by running typeof command that "2" is a string and its still converting it into number.
+ 
+
+// ===  strict check with triplr equals === 
+console.log("2" === 2);
+console.log(typeof "2");
+
+// by doing strict check (===) javascript will not convert string into number automatically..
+
