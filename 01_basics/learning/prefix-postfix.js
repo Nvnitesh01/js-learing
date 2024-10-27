bdba92efb54d08ac2e554f1bc687d91375941f81
Nvnitesh01 <54271273+Nvnitesh01@users.noreply.github.com>
2024-10-27 09:32:49 +0000

    prepost

---
+++ 01_basics/learning/prefix-postfix.js
@@ -0,0 +1,14 @@
+/*Prefix Examples */
+
+// let pre = 10;
+// console.log(pre);
+// console.log(++pre);  // ans = 11.. bcos we added ++ before pre, so its value is incresed by plus 1
+// console.log(--pre);  // ans = 10.. after adding plus 1 from from previous console log we got ans 11. But when we added -- before pre its value is again become 10, bcos -- substrated -1 from the last console answer which was 11.
+
+/*Postfix Examples */
+
+let post = 10;
+console.log(post);
+console.log(post--); // ans = 10.. value remains the same because we have used postfix means -- after the value, but the next console log value would be 9
+console.log(post);  // ans = 9.. its bcos the previous console log uses -- after post. so the next console answer would be 10-1 = 9.
+
